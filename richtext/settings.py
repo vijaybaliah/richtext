370b0491c1db3c2044be16509cdcfcfedf89460d
vijaybaliah <vijaybalu1989@gmail.com>
2022-09-08 01:50:13 +0530

    feat(templates): added text editor templates

--- richtext/settings.py
+++ richtext/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'richtext.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATE_DIR
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
